4a083f3d4058b479d9b3e71bd192685394e21418
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-06-19 13:16:39 +0000

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+"""
 
-"""This script starts the flask application and runs the web server."""
-
-from flask import Flask
-from flask import render_template
-from models import storage
+"""
+from flask import Flask, render_template
 from models.state import State
+from models import storage
 
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def teardown_session(_):
-    """Closes the current session after each request."""
+def teardown(exception):
+    """
+
+    """
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def get_states():
-    """Renders all the State objects available in the storage."""
-    states = storage.all(State).values()
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
 
-    return render_template("7-states_list.html", states=states)
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
